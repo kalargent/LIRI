69f2e32b4bf18e76c785bca99cb32edb68f33cdd
Karen Largent <karenlargent@Karens-MacBook-Pro.local>
2019-06-27 15:56:45 -0400

    functioning spotify and omdb API calls using switch case

--- liri.js
+++ liri.js
@@ -4,7 +4,12 @@ var keys = require("./keys");
 
 var Spotify = require('node-spotify-api');
 
-var spotify = new Spotify(keys.spotify);   
+var spotify = new Spotify(keys.spotify); 
+
+var omdbKey = "trilogy"; 
+
+var axios = require("axios"); 
+
 
 // spotify.search({type: "track", query: "phoenix"}, function(err, data){ 
 //     if (err) {
@@ -13,16 +18,66 @@ var spotify = new Spotify(keys.spotify);
 
 //     console.log(data); 
 // })
+var command = process.argv[2];
+var searchTerm = process.argv[3]; 
 
+function spotifySearch (searchTerm) {
 spotify
-  .search({ type: 'track', query: 'Wilson', limit: 1 })
+  .search({ type: 'track', query: searchTerm, limit: 1 })
   .then(function(response) {
     console.log(response);
     console.log("==========================")
     console.log(response.tracks.items); 
     console.log("==========================")
-    console.log(response.tracks.items.album);
+    console.log(response.tracks.items[0].name);
   })
   .catch(function(err) {
     console.log(err);
   });
+}
+
+function omdbSearch (searchTerm){ 
+  axios.get (`http://www.omdbapi.com/?t=${searchTerm}&apikey=${omdbKey}`)
+  .then (
+    function(response) {
+      console.log("The movie's rating is: " + response.data.imdbRating);
+    }
+  )
+
+
+  .catch(function(error) {
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.log("---------------Data---------------");
+      console.log(error.response.data);
+      console.log("---------------Status---------------");
+      console.log(error.response.status);
+      console.log("---------------Status---------------");
+      console.log(error.response.headers);
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an object that comes back with details pertaining to the error that occurred.
+      console.log(error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.log("Error", error.message);
+    }
+    console.log(error.config);
+  });
+
+
+}
+
+
+  switch (command) {
+    case "spotify": 
+    spotifySearch(searchTerm); 
+    break; 
+
+    case "movie": 
+    omdbSearch(searchTerm); 
+    break; 
+  }
+
+
