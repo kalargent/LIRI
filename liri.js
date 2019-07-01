be65256d72731eeb9d89aa72caa78db8c9e05f68
Karen Largent <karenlargent@Karens-MacBook-Pro.local>
2019-07-01 16:26:21 -0400

    fixed error from the bands in town API by adding .catch

--- liri.js
+++ liri.js
@@ -84,12 +84,35 @@ function omdbSearch (searchTerm){
 }
 
 function concertSearch (searchTerm){ 
-  axios.get(`https://rest.bandsintown.com/artists/${searchTerm}/events?app_id=codingbootcamp?limit=5`) 
+  axios.get(`https://rest.bandsintown.com/artists/${searchTerm}/events?app_id=codingbootcamp`) 
   .then (
     function (response) {
       console.log(response.data); 
+      console.log("==========================")
+      console.log("Venue: " + response.data.venue[0]); 
+      console.log("==========================")
     }
   )
+  .catch(function(error) {
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.log("---------------Data---------------");
+      console.log(error.response.data);
+      console.log("---------------Status---------------");
+      console.log(error.response.status);
+      console.log("---------------Status---------------");
+      console.log(error.response.headers);
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an object that comes back with details pertaining to the error that occurred.
+      console.log(error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.log("Error", error.message);
+    }
+    console.log(error.config);
+  });
 }
 
 
