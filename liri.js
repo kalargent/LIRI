85a64b7947c3f2463c42deaf0ee78e7637df9f93
Karen Largent <karenlargent@Karens-MacBook-Pro.local>
2019-07-01 17:57:19 -0400

    limited to 5 concert results

--- liri.js
+++ liri.js
@@ -87,7 +87,7 @@ function concertSearch (searchTerm){
   axios.get(`https://rest.bandsintown.com/artists/${searchTerm}/events?app_id=codingbootcamp`) 
   .then (
     function (response) {
-      for (i=0; i < 10; i++) {
+      for (i=0; i < 5; i++) {
       // console.log(response.data); 
       console.log("==========================")
       console.log("Date: " + response.data[i].datetime); 
